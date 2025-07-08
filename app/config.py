b5a0f9c6cd4c188fb9f55f103683a08a2a5c96a2
Hammad Wakeel <113436675+Hammadwakeel@users.noreply.github.com>
2025-07-08 15:41:46 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,13 +7,11 @@ load_dotenv()
 class Settings:
     # MongoDB
     MONGO_USERNAME = os.getenv("MONGO_USERNAME")
-    MONGO_PASSWORD = quote_plus(os.getenv("MONGO_PASSWORD"))  # Escape special characters
+    MONGO_PASSWORD = quote_plus(os.getenv("MONGO_PASSWORD"))  # Encode special chars
     DATABASE_NAME = os.getenv("DATABASE_NAME")
     COLLECTION_NAME = os.getenv("COLLECTION_NAME")
-    CONNECTION_STRING = os.getenv("CONNECTION_STRING_TEMPLATE").format(
-        username=MONGO_USERNAME,
-        password=MONGO_PASSWORD
-    )
+
+    CONNECTION_STRING = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@cluster0.ywbtfhf.mongodb.net/{DATABASE_NAME}?retryWrites=true&w=majority"
 
     # Security
     SECRET_KEY = os.getenv("SECRET_KEY")
